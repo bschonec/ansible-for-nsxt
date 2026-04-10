19ad332233fb91afab97a58c11f8d2b915c4db85
Brian Schonecker <bschonec@gmail.com>
2026-04-10 13:24:13 -0400

    It may be that your whole purpose in life is simply to serve as a warning to others.

--- plugins/modules/nsxt_group.py
+++ plugins/modules/nsxt_group.py
@@ -92,7 +92,7 @@ def get_group_with_display_name(module, manager_url, mgr_username, mgr_password,
   '''
   result: returns the group object with the display name provided
   '''
-  groups = get_groups(module, manager_url, mgr_username, mgr_password, validate_certs, domain, display_name)
+  certificates = get_groups(module, manager_url, mgr_username, mgr_password, validate_certs, domain, display_name)
   for certificate in certificates['results']:
      if certificate.__contains__('display_name') and certificate['display_name'] == display_name:
         return certificate
