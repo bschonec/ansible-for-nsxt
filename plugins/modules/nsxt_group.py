1895d8c7ff33d4a9571416fbd2db7b08004a62ba
Brian Schonecker <bschonec@gmail.com>
2026-04-10 13:12:31 -0400

    There are worse things in life than death.  Have you ever spent an evening with an insurance salesman? 		-- Woody Allen

--- plugins/modules/nsxt_group.py
+++ plugins/modules/nsxt_group.py
@@ -101,12 +101,12 @@ def get_group_with_display_name(module, manager_url, mgr_username, mgr_password,
 def main():
   argument_spec = vmware_argument_spec()
   argument_spec.update(
-    dict(
-      state=dict(required=True, choices=['present', 'absent']),
-                         domain        = dict(type='str', default='default'),
-                         validate_certs= dict(type='bool', requried=False, default=True),
-                         description=dict(required=False, type='str'),
-                         display_name=dict(required=False, type='str'),
+      dict(
+          state=dict(required=True, choices=['present', 'absent']),
+          domain=dict(type='str', default='default'),
+          validate_certs=dict(type='bool', required=False, default=True),
+          description=dict(required=False, type='str'),
+          display_name=dict(required=False, type='str'),
       )
   )
 
