fdc7594613b594a3ab94c73fedb27ddaec983492
Brian Schonecker <bschonec@gmail.com>
2026-04-10 09:13:01 -0400

    The Blue Screen Of Advocacy
    The Federal Bureau of Investigation & Privacy Violations has issued a
    national advisory warning computer stores to be on the lookout for the
    "Bluescreen Bandits". These extreme Linux zealots go from store to store
    and from computer to computer typing in "C:\CON\CON" and causing the demo
    machines to crash and display the Blue Screen Of Death.
    
    Efforts to apprehend the bandits have so far been unsuccessful. The
    outlaws were caught on tape at a CompUSSR location in Southern California,
    but in an ironic twist, the surveillance system bluescreened just before
    the penguinistas came into clear view.
    
    "We don't have many clues. It's not clear whether a small group is behind
    the bluescreen vandalism, or whether hundreds or even thousands of geek
    zealots are involved," said the manager of a Capacitor City store.
    
    The manager has good reason to be upset. The bluescreen raid was the top
    story in the local newspaper and quickly became a hot topic of discussion.
    As a result, the local school board halted its controversial plans to
    migrate their computers from Macs to PCs.

--- plugins/modules/nsxt_group_facts.py
+++ plugins/modules/nsxt_group_facts.py
@@ -77,6 +77,7 @@ def main():
   mgr_hostname = module.params['hostname']
   mgr_username = module.params['username']
   mgr_password = module.params['password']
+  validate_certs = module.params['validate_certs']
   mgr_domain = module.params['domain']
 
   manager_url = 'https://{}/policy/api/v1'.format(mgr_hostname)
