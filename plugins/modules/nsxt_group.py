096afd0397ea823eae706bd3d6c81e45514f14db
Brian Schonecker <bschonec@gmail.com>
2026-04-10 14:14:54 -0400

    It seems there's this magician working one of the luxury cruise ships for a few years.  He doesn't have to change his routines much as the audiences change over fairly often, and he's got a good life.   The only problem is the ship's parrot, who perches in the hall and watches him night after night, year after year.  Finally, the parrot figures out how almost every trick works and starts giving it away for the audience.  For example, when the magician makes a bouquet of flowers disappear, the parrot squawks "Behind his back!  Behind his back!"  Well, the magician is really annoyed at this, but there's not much he can do about it as the parrot is a ship's mascot and very popular with the passengers. 	One night, the ship strikes some floating debris, and sinks without a trace.  Almost everyone aboard was lost, except for the magician and the parrot.  For three days and nights they just drift, with the magician clinging to one end of a piece of driftwood and the parrot perched on the other end. As the sun rises on the morning of the fourth day, the parrot walks over to the magician's end of the log.  With obvious disgust in his voice, he snaps "OK, you win, I give up.  Where did you hide the ship?"

--- plugins/modules/nsxt_group.py
+++ plugins/modules/nsxt_group.py
@@ -140,7 +140,7 @@ def main():
       })
       (rc, resp) = request(manager_url+ '/infra/domains/' + domain + '/groups/' + display_name, data=foo, headers=headers, method='PATCH',
                               url_username=mgr_username, url_password=mgr_password, validate_certs=validate_certs, ignore_errors=True)
-      module.fail_json(msg="Group with display name \'%s\' already exists." % display_name)  
+      #module.fail_json(msg="Group with display name \'%s\' already exists." % display_name)  
     try:
       headers = dict(Accept="application/json")
       headers['Content-Type'] = 'application/json'
