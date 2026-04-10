efd952e81785f180ccc0c7224b98b3d21419a5a5
Brian Schonecker <bschonec@gmail.com>
2026-04-10 09:08:59 -0400

    Periphrasis is the putting of things in a round-about way.  "The cost may be upwards of a figure rather below 10m#." is a periphrasis for The cost may be nearly 10m#.  "In Paris there reigns a complete absence of really reliable news" is a periphrasis for There is no reliable news in Paris.  "Rarely does the 'Little Summer' linger until November, but at times its stay has been prolonged until quite late in the year's penultimate month" contains a periphrasis for November, and another for lingers.  "The answer is in the negative" is a periphrasis for No.  "Was made the recipient of" is a periphrasis for Was presented with.  The periphrasis style is hardly possible on any considerable scale without much use of abstract nouns such as "basis, case, character, connexion, dearth, description, duration, framework, lack, nature, reference, regard, respect".  The existence of abstract nouns is a proof that abstract thought has occurred; abstract thought is a mark of civilized man; and so it has come about that periphrasis and civilization are by many held to be inseparable.  These good people feel that there is an almost indecent nakedness, a reversion to barbarism, in saying No news is good news instead of "The absence of intelligence is an indication of satisfactory developments." 		-- Fowler's English Usage

--- plugins/modules/nsxt_group_facts.py
+++ plugins/modules/nsxt_group_facts.py
@@ -68,6 +68,7 @@ from ansible.module_utils._text import to_native
 
 def main():
   argument_spec = vmware_argument_spec()
+  argument_spec.update ( mgr_domain = dict(type='str', default='domain'))
 
   module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
 
@@ -76,11 +77,6 @@ def main():
   mgr_password = module.params['password']
   mgr_domain = module.params['domain']
 
-  argument_spec.update (
-    dict(
-      mgr_domain = dict(type='str', default='domain')
-    )
-  )
   manager_url = 'https://{}/policy/api/v1'.format(mgr_hostname)
 
   changed = False
